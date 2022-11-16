f14b185b8fafec7c2b91d6d0c8ad53a06b8c75fb
Fabian <arevalo.cpweb@gmail.com>
2022-11-16 12:08:28 +0000

    first commit

---
+++ practicaFinal.py
@@ -0,0 +1,29 @@
+import os
+from time import sleep
+from datetime import datetime
+
+def fecha():
+    now = datetime.now()
+    return "%d:%d:%d" %(now.hour,now.minute,now.second)
+
+def padre():
+    for x in range(10):
+        
+        sleep(10)
+
+        pid = os.fork()
+
+        if(pid != 0):
+            print("Iniciando el proceso:",os.getpid(),"a las",fecha())
+        else:
+            hijo()
+
+def hijo():
+    
+    print("Iniciando el proceso con PID:",os.getpid())
+    sleep(3)
+    print("Terminando el proceso con PID:",os.getpid())
+    os._exit(1)
+
+if __name__ == "__main__":
+    padre()
